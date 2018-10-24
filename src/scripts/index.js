241d08bea1f1a734360225af010be1e782fd04b4
Rupert Parry <rupertp@google.com>
2018-10-24 14:07:37 +1100

    Added reset styles and better HTML boilerplate.

--- src/scripts/index.js
+++
@@ -1,8 +0,0 @@
-// import handlebars from 'handlebars';
-// import { readFileSync } from 'fs'
-
-// const template = readFileSync(__dirname + '/../views/partials/test.hbs', 'utf-8');
-// console.log(template);
-
-import examplePartial from '../partials/example_partial.hbs';
-document.querySelector("#app").innerHTML = examplePartial({ "hello": "Hello World!" });
